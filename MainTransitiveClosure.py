f9b22c59384b5cf24f2e98296edfafdc4ddcaf71
PingMaster99 <rui18259@uvg.edu.gt>
2020-08-13 22:25:10 -0600

    Docstring Final Files

--- MainTransitiveClosure.py
+++ MainTransitiveClosure.py
@@ -7,19 +7,22 @@
 	Date: Aug 14, 2020
 """
 
-from TransitiveClosure import transitive_closure,warshall_algorithm
+from TransitiveClosure import transitive_closure, warshall_algorithm
 
-# Se define la matriz de adyacencia del grafo 
+# Adjacency matrix for the relationship
 R = [
-	[0, 0, 1, 1],		# 1
-	[1, 0, 0, 0],		# 2
-	[0, 1, 0, 0],		# 3
-	[0, 0, 0, 0],		# 4
+    [0, 0, 1, 1],  # 1
+    [1, 0, 0, 0],  # 2
+    [0, 1, 0, 0],  # 3
+    [0, 0, 0, 0],  # 4
 ]
 
-print("     Programa para hacer el cierre transitivo de matrices booleanas \n")
-print("     El resultado del cierre transitivo es el siguiente: \n")
+# Program prints the transitive closure results of the relationship
+print("*******************************************************************\n  Programa para hacer el cierre "
+      "transitivo de matrices booleanas\n*******************************************************************")
+print("Por: Douglas de León, Isabel Ortiz y Pablo Ruiz\n\n")
+print("El resultado del cierre transitivo es el siguiente:")
 transitive_closure(R)
-print(      "Programa que usa el algoritmo de Warshall para hacer el cierre transitivo de una relacion \n")
+print("\nPrograma que usa el algoritmo de Warshall para hacer el cierre transitivo de una relacion")
 warshall_algorithm(R)
-
+print("\nGracias por usar el programa de cierre transitivo, que tenga un feliz día :)")
